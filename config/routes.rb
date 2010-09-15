610500efd65a79eb3f3c356f5765c73f3b841160
explosiveamber <mail.roma1@gmail.com>
2010-09-15 22:12:29 +0200

    Update: Now Ended Chapter 3

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ SampleApp::Application.routes.draw do
 
   get "pages/about"
   
+  get "pages/help"
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,48 +1,55 @@
 require 'spec_helper'
 
 describe PagesController do
-  include Rspec::Rails::ControllerExampleGroup
-  render_views
+render_views
 
-  describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
-    end
-  
-  
-    it "should have the right title" do
-      get 'home'
-      response.should have_selector("title",
-      :content => "Ruby on Rails Tutorial Sample App | Home")
-  end
+before(:each) do
+
+@base_title = "Ruby on Rails Tutorial Sample App"
+
 end
 
-  describe "GET 'contact'" do
-    it "should be successful" do
-      get 'contact'
-      response.should be_success
-    end
-    
-    it "should have the right title" do
+describe "GET 'home'" do
+  
+it "should be successful" do
+get 'home'
+response.should be_success
+end
+
+it "should have the right title" do
+get 'home'
+response.should have_selector("title",
+:content => @base_title + " | Home")
+end
+
+end
+
+describe "GET 'contact'" do
+  
+it "should be successful" do
+get 'contact'
+response.should be_success
+end
+
+it "should have the right title" do
 get 'contact'
 response.should have_selector("title",
-:content =>
-"Ruby on Rails Tutorial Sample App | Contact")
-  end
+:content => @base_title + " | Contact")
 end
- 
- describe "GET 'about'" do
+
+end
+
+describe "GET 'about'" do
+  
 it "should be successful" do
 get 'about'
 response.should be_success
-    end
-  it "should have the right title" do
+end
+
+it "should have the right title" do
 get 'about'
 response.should have_selector("title",
-:content =>
-"Ruby on Rails Tutorial Sample App | About")
-    end
-  end
-  
+:content => @base_title + " | About")
+end
+end
 end
